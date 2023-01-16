5d5a1b3568cfdc22613831e5296aed473e31add2
obimoro <122788971+obimoro@users.noreply.github.com>
2023-01-16 22:54:22 +0100

    minor update
    Working latenight trying to get the cells to work. Managed to buchter something together. Off to bed

--- code/level.py
+++ code/level.py
@@ -7,10 +7,65 @@ class Level:
     def __init__(self, screen):
         self.screen = screen
         # create a array in a certain size with the vaule 0 or 1
-        self.map = np.random.randint(2, size=(16,16))
+        self.map = np.random.randint(2, size=(ARR_H,ARR_W))
 
+    def update(self):
+        self.map = np.random.randint(2, size=(ARR_H,ARR_W))
+        return self.map
 
     def display(self):
+        # Creates a copy of previous array with its content
+        self.next_map = self.map.copy()
+
+        for x in range(ARR_W - 1):
+            for y in range(ARR_H - 1):
+
+                num = 0
+                #if self.next_map[x - 1][y - 1] == 1:
+                #    num += 1
+                #if self.next_map[x][y - 1] == 1:
+                #    num += 1
+                #if self.next_map[x + 1][y - 1] == 1:
+                #    num += 1
+                #if self.next_map[x - 1][y] == 1:
+                #    num += 1
+                #if self.next_map[x + 1][y] == 1:
+                #    num += 1
+                #if self.next_map[x - 1][y + 1] == 1:
+                #    num += 1
+                #if self.next_map[x][y + 1] == 1:
+                #    num += 1
+                #if self.next_map[x + 1][y + 1] == 1:
+                #    num += 1
+                if self.next_map[y - 1][x - 1] == 1:
+                    num += 1
+                if self.next_map[y][x - 1] == 1:
+                    num += 1
+                if self.next_map[y + 1][x - 1] == 1:
+                    num += 1
+                if self.next_map[y - 1][x] == 1:
+                    num += 1
+                if self.next_map[y + 1][x] == 1:
+                    num += 1
+                if self.next_map[y - 1][x + 1] == 1:
+                    num += 1
+                if self.next_map[y][x + 1] == 1:
+                    num += 1
+                if self.next_map[y + 1][x + 1] == 1:
+                    num += 1
+
+                
+                if self.next_map[x][y] == 1 and (num == 2 or num == 3):
+                    self.next_map[x][y] = 1
+                elif self.next_map[x][y] == 0 and num == 3:
+                    self.next_map[x][y] = 1
+                else:
+                    self.next_map[x][y] = 0    
+                print(num)
+                self.map = self.next_map
+        return self.map
+
+    def render(self):
         # Goes throught each element and draws a Rect, 0 (dead = white), 1 (alive = black)
         for row_index, rows in enumerate(self.map):
             for col_index, cols in enumerate(rows):
@@ -20,24 +75,5 @@ class Level:
                     pg.draw.rect(self.screen, (255,255,255), (x, y, TILESIZE - TILE_OFFSET,TILESIZE - TILE_OFFSET))
                 elif cols == 1:
                     pg.draw.rect(self.screen, (0,0,0),  (x,y, TILESIZE - TILE_OFFSET,TILESIZE - TILE_OFFSET))
-        
-        # Creates a copy of previous array with its content
-        self.next_map = self.map.copy()
 
-        for row_index, rows in enumerate(self.next_map):
-            for col_index, cols in enumerate(rows):
-                x = col_index
-                y = row_index
-                 
-
-                 #Go through neighbours
-                neighboursAlive = 0
-                neighboursAlive += self.next_map[4][4]
-                neighboursAlive += self.next_map[4][4 - 1]
-                neighboursAlive += self.next_map[4 - 1][4]
-                print(neighboursAlive)
-                if neighboursAlive >= 2:
-                    self.next_map[4][4] = 1
-                elif neighboursAlive <= 2:
-                    self.next_map[4][4] = 0
-                self.map = self.next_map
+        self.display()
--- code/main.py
+++ code/main.py
@@ -12,21 +12,29 @@ class App:
         # setting up pygame screen
         self.screen = pg.display.set_mode((SCR_WIDTH,SCR_HEIGHT))
         self.level = Level(self.screen)
+        # Timing
+        self.clock = pg.time.Clock()
+
+
         
     # event handler og pygame
     def check_event(self):
         for event in pg.event.get():
             # checks if window is being closed and then exits gracefully 
-             if event.type == pg.QUIT  or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            if event.type == pg.QUIT  or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
-
+            if event.type == pg.KEYDOWN and event.key == pg.K_r:
+                self.level.update()
+            if event.type == pg.KEYDOWN and event.key == pg.K_e:
+                self.level.display()
+                
     # render to the screen
     def render(self):
         # fills the windows with color (orange) and then,
         # updates the screen
         self.screen.fill((255,125,0))
-        self.level.display()
+        self.level.render()
         #pg.draw.rect(self.screen, (0,0,0), (200,150,100,50))
         pg.display.flip()
 
@@ -35,6 +43,7 @@ class App:
         while True:
             self.check_event()
             self.render()
+            self.clock.tick(60)
 
 # checks if current file is correct and creates an object of the 
 # application class, then runs
