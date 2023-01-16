79988bd440112c10441e73de737f12da880b1230
obimoro <122788971+obimoro@users.noreply.github.com>
2023-01-16 14:23:35 +0100

    minor update
    defined functions and commented them respectively. Filled out short description of Conways game of life and its rules in the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Conway-s-Game-of-life
-Conway's Game of life in python
+Conway's Game of life also known simply as Life, is a zero-player game is a cellular automaton dvised by John Horton Conway in 1970. 
+
+
+The universe of Life is a infinite, 2d orthogonal grid of square cells. Where each cell has only 2 possible states, live or dead (populated or unpopulated). Every cell can interact with its 8 neighbouring cells in horizontal, vetical or diagonal. Each step in time the following will happen:
+
+1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
--- code/main.py
+++ code/main.py
@@ -1,17 +1,36 @@
 import pygame as pg
+import sys
 
 class App:
     def __init__(self):
+        # initialize pygame
         pg.init()
+        # setting up pygame screen
+        self.screen = pg.display.set_mode((800,600))
+        
+    # event handler og pygame
+    def check_event(self):
+        for event in pg.event.get():
+            # checks if window is being closed and then exits gracefully 
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
 
+    # render to the screen
+    def render(self):
+        # fills the windows with color (orange) and then,
+        # updates the screen
+        self.screen.fill((255,125,0))
+        pg.display.flip()
 
-
-    def update(self):
-        ...
-
+    # Applications run loop
     def run(self):
-        ...
+        while True:
+            self.check_event()
+            self.render()
 
+# checks if current file is correct and creates an object of the 
+# application class, then runs
 if __name__ == '__main__':
     game = App()
     game.run()
