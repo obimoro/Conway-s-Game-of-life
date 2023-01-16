20f1998065ec6cbab5cfc2fd28b2be35ab007245
obimoro <122788971+obimoro@users.noreply.github.com>
2023-01-16 14:07:10 +0100

    Quick Update
    Init code for the application

---
+++ code/main.py
@@ -0,0 +1,17 @@
+import pygame as pg
+
+class App:
+    def __init__(self):
+        pg.init()
+
+
+
+    def update(self):
+        ...
+
+    def run(self):
+        ...
+
+if __name__ == '__main__':
+    game = App()
+    game.run()
